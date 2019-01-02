880175758252e4727d332f912ba74658f5832f67
punkrocker <yuhaokui@yuhaokui.com>
2019-01-02 21:38:18 +0800

    Add panel componet

---
+++ src/componets/Panel.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Panel extends Component {
+    render() {
+        return (
+            <div style={{height: 100, width:100, background: 'green'}}></div>
+        );
+    }
+}
+
+export default Panel;
